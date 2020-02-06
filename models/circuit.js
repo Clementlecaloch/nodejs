ec0e64d8cb0a5799077503a7b95907d0f5478dd2
Valentin Jeanjon <valentin.jeanjon@etu.unilim.fr>
2020-02-06 08:54:05 +0100

    circuit menu - Ok

---
+++ models/circuit.js
@@ -0,0 +1,17 @@
+let db = require('../configDb');
+
+module.exports.getListeCircuit = function (callback) {
+   // connection à la base
+	db.getConnection(function(err, connexion){
+        if(!err){
+        	  // s'il n'y a pas d'erreur de connexion
+        	  // execution de la requête SQL
+						let sql ="SELECT cirnum, payadrdrap, cirnom FROM circuit c INNER JOIN pays p ON p.paynum=c.paynum ORDER BY cirnom";
+						//console.log (sql);
+            connexion.query(sql, callback);
+
+            // la connexion retourne dans le pool
+            connexion.release();
+         }
+      });
+};
