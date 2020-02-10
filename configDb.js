78d59ff08d04fd898f620c2363d35f75d081e560
Valentin Jeanjon <valentin.jeanjon@etu.unilim.fr>
2020-02-10 20:59:49 +0100

    Modification arborescence

--- configDb.js
+++ configDb.js
@@ -14,13 +14,13 @@ let pool  = mysql.createPool({
   user     : 'bd',
   password : 'bede',
   database : 'grandprix',
-    port: '8889'
+  port     : '8889'
 });
 
 module.exports.getConnection = function(callback) {
     pool.getConnection(function(err, connection) {
         callback(err, connection);
-        
+
     });
 
 };
