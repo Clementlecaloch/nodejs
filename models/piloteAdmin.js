1f05d6dc7b8f35871c4f6708038a64e5faf2c9c4
Clement Le Caloch <clement.le-caloch@etu.unilim.fr>
2020-02-19 14:47:13 +0100

    pilote admin

---
+++ models/piloteAdmin.js
@@ -0,0 +1,62 @@
+let db = require('../configDb');
+
+module.exports.getListePilote = function (callback) {
+    // connection à la base
+    db.getConnection(function(err, connexion){
+        if(!err){
+            // s'il n'y a pas d'erreur de connexion
+            // execution de la requête SQL
+            let sql = " SELECT pilnum, pilnom, pilprenom, pildatenais FROM pilote";
+            //console.log (sql);
+            connexion.query(sql, callback);
+
+            // la connexion retourne dans le pool
+            connexion.release();
+        }
+    });
+};
+
+module.exports.getNationalitePilote = function (callback) {
+    // connection à la base
+    db.getConnection(function(err, connexion){
+        if(!err){
+            // s'il n'y a pas d'erreur de connexion
+            // execution de la requête SQL
+            let sql = "SELECT paynum, paynat FROM pays";
+            //console.log (sql);
+            connexion.query(sql, callback);
+
+            // la connexion retourne dans le pool
+            connexion.release();
+        }
+    });
+};
+
+module.exports.getNomEcurie = function (callback) {
+    // connection à la base
+    db.getConnection(function(err, connexion){
+        if(!err){
+            // s'il n'y a pas d'erreur de connexion
+            // execution de la requête SQL
+            let sql = "SELECT ecunum, ecunom FROM ecurie";
+            //console.log (sql);
+            connexion.query(sql, callback);
+
+            // la connexion retourne dans le pool
+            connexion.release();
+        }
+    });
+};
+
+module.exports.getInfoPilote = function (num, callback) {
+    db.getConnection(function (err,connexion) {
+        if(!err) {
+            let sql = "select phoadresse, pilnom, pilprenom, pildatenais, pilpoids, piltaille, ecunom, paynom, piltexte from pilote p " +
+                "left join ecurie e on p.ecunum = e.ecunum join photo ph on p.pilnum = ph.pilnum join pays pa " +
+                " on pa.paynum = p.paynum where ph.phonum = 1 and p.pilnum = " + num;
+            connexion.query(sql,callback);
+            connexion.release();
+        }
+    });
+};
+
--- router/routerAdmin.js
+++ router/routerAdmin.js
@@ -1,4 +1,5 @@
 let HomeAdminController = require('./../controllers/HomeAdminController');
+let PiloteAdminController = require('./../controllers/PiloteAdminController');
 
 // Routes
 module.exports = function(app){
@@ -6,6 +7,12 @@ module.exports = function(app){
 // Main Routes
     app.get('/', HomeAdminController.Index);
     app.post('/', HomeAdminController.Connexion);
+
+    // Routes Pilotes
+    app.get('/piloteAdmin', PiloteAdminController.Index);
+    app.get('/ajouterPilote', PiloteAdminController.Ajouter);
+    app.get('/modifierPilote/:num', PiloteAdminController.Modifier);
+
 
 // tout le reste
 app.get('*', HomeAdminController.NotFound);
